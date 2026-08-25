c3c1f92bfd1bff9daf69e547578586e73168d448
Caseiro <guilherme.a.caseiro@gmail.com>
2026-08-25 16:59:23 -0300

    feat: Entrada de Dados concluido

--- SysExpressTicketFestivalCidade/Program.cs
+++ SysExpressTicketFestivalCidade/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace FestivalCidade
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("===Sistema de Ingresso Express Festival Cidade===");
+
+            Console.Write("Nome do Fã: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Quantidade de Dias de Show: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Console.Write("Cupom: ");
+            string cupom = Console.ReadLine();
+
+            Console.Write("Pontuação Fidelidade: ");
+            int pontos = int.Parse(Console.ReadLine());
+        }
+    }
+}
